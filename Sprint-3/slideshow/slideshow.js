92b504e560c7b99b0a46c98b1815fa4ae8089d8d
Imran Mohamed <info@imranbmohamed.com>
2025-11-08 01:30:00 +0000

    Refactor slideshow HTML structure and enhance JavaScript functionality for image carousel

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -6,3 +6,32 @@ const images = [
 
 
 // Write your code here
+
+//dom elements
+const image = document.getElementById("carousel-img");
+const forwardBtn = document.getElementById("forward-btn");
+const backwardBtn = document.getElementById("backward-btn");
+
+//state
+let currentIndex = 0;
+
+//event listeners
+forwardBtn.addEventListener("click", () => {
+    currentIndex++;
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+    image.src = images[currentIndex];
+});
+
+backwardBtn.addEventListener("click", () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    }
+    image.src = images[currentIndex];
+});
+
+window.onload = () => {
+    image.src = images[currentIndex];
+};
